30a0ffad9cd725aa0ed01c8951625b62d01fe36b
PrayaagM <prayaagmunshi@gmail.com>
2023-05-16 17:37:25 -0400

    Refactoring

---
+++ Prayaag/events_callbacks/refactoredWithPromises.js
@@ -0,0 +1,49 @@
+
+const wait = (ms = 0) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms);
+    })
+}
+
+const animate = (e) => {
+    const box = e.currentTarget;
+    wait(1000)
+    .then(() => {
+        box.innerHTML = "Clicked!";
+        return wait(1000);
+    })
+    .then(() => {
+        box.classList.add("circle");
+        return wait(1000);
+    })
+    .then(() => {
+        box.classList.add("pink");
+        return wait(1000);
+    })
+    .then(() => {
+        box.classList.add("fade-away");
+        return wait(1000);
+    })
+    .then(() => {
+        box.classList.add("fade-in");
+        return wait(1000);
+    })
+    .then(() => {
+        box.classList.remove("circle");
+        return wait(1000);
+    })
+    .then(() => {
+        box.classList.remove("pink");
+        return wait(1000);
+    })
+    .then(() => {
+        box.classList.remove("fade-out");
+        box.innerHTML = "Click Me!";
+        console.log("Done Animating");
+    })
+    .catch(err => console.error(err));
+
+}
+
+
+box.addEventListener('click', animate);
