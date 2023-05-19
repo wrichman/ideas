77973cf144322d5ea0f7554d4a103b401e7e7748
Mia99Kawaii <133615712+Mia99Kawaii@users.noreply.github.com>
2023-05-19 01:35:23 -0400

    Add files via upload

---
+++ Sitong/basic practice/the-dom.js
@@ -0,0 +1,6 @@
+const p = document.querySelector('p');
+const imgs = document.querySelectorAll('.item img');
+const item2 = document.querySelector('.item2');
+const item2Image = item2.querySelector('img');
+const heading = document.querySelector('h2');
+console.log(heading);
