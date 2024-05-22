7628ae24d05f03e087bb04d486a87646f76cf8b7
“Chris <chrisalexanderpop@gmail.com>
2024-05-22 13:44:52 -0400

    Finish app code

--- Chris/react-beginner-catch-of-the-day/src/components/StorePicker.js
+++ Chris/react-beginner-catch-of-the-day/src/components/StorePicker.js
@@ -1,12 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
   myInput = React.createRef();
+  static propTypes = {
+    history: PropTypes.object
+  };
 
   goToStore = event => {
+    // 1. Stop the form from submitting
     event.preventDefault();
+    // 2. get the text from that input
     const storeName = this.myInput.current.value;
+    // 3. Change the page to /store/whatever-they-entered
     this.props.history.push(`/store/${storeName}`);
   };
   render() {
